c8cc9462a12f89b5a989ab28a6733b187261b08d
yanndrade <ybamello@hotmail.com>
2023-10-20 17:17:32 +0200

    Adding visualization,and making move| TODO: making move appears on the visualization, add pheromones

--- main.py
+++ main.py
@@ -4,8 +4,34 @@ from src.ant import *
 
 if __name__ == "__main__":
 
-    cologne = ant_cologne(10)
-    for n in range(2):
-        print("-----------------------")
-        cologne.step()
+    # positions = []
+
+    # cologne = ant_cologne(10,5,5)
+
+    # print_grid(cologne)
+
+    # for cell in cologne.grid.coord_iter():
+    #     cell_content, x, y = cell
+    #     if (cologne.grid.is_cell_empty((x,y))):
+    #        pass
+    #     else: 
+    #         for ant in cell_content:
+    #             ant.move()
+
+    
+
+    # print_grid(cologne)
+
+    #cologne.step()
+
+    grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+    server = mesa.visualization.ModularServer(
+    ant_cologne, [grid], "Ant Cologne", {"num": 10, "L": 5, "H": 5}
+    )
+    # server = ModularServer(ant_cologne,
+    #     [grid],
+    #     "Ant cologne",
+    #     {"num": 10, "L": 5, "H": 5  })
+    server.port = 8521 # The default
+    server.launch()
     
--- src/ant.py
+++ src/ant.py
@@ -3,6 +3,9 @@
 import mesa
 from mesa.model import Model
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class ant_agent(mesa.Agent):
     """Ant agent"""
@@ -10,26 +13,96 @@ class ant_agent(mesa.Agent):
     def __init__(self, unique_id: int, model: Model) -> None:
         super().__init__(unique_id, model)
 
-        self.phero = 1
+        #self.phero = 1
+
+        self.coords = (0,0)
+
+
+    def move(self):
+        step = self.model.grid.get_neighborhood(
+            self.coords,
+            moore = False,
+            include_center=False
+        )
+        new_step = self.random.choice(step)
+
+        print(f"Ant:{self.unique_id} moving from {self.coords} to {new_step}")
+        
+        self.model.grid.move_agent(self, new_step)
 
     def step(self):
-        print(f"Hi i am a ant number {self.unique_id} and i have {self.phero} pheromones")
+        print(self.model.phero)
+        
+
+        #print(f"Hi i am a ant number {self.unique_id} and i have {self.phero} pheromones")
 
 
 
 class ant_cologne(mesa.Model):
 
-    def __init__(self, num):
+    def __init__(self, num, L, H):
 
         self.num_agents = num
         self.schedule = mesa.time.RandomActivation(self)
+        self.grid = mesa.space.MultiGrid(L,H,torus=True)
+        self.phero = 0
 
         for i in range(self.num_agents):
             ant = ant_agent(i,self)
             self.schedule.add(ant)
+            coords= (self.random.randrange(0,L),self.random.randrange(0,H))
+            ant.coords = coords
+
+            self.grid.place_agent(ant,coords)
+        
+
+    
 
     def step(self):
+        ## Add phero into a cell if the cell has an ant
+        for cell in self.grid.coord_iter():
+            cell_content,x,y = cell
+            if len(cell_content)!=0:
+                print(f"cell content: {cell_content}\n")
+                self.phero = 1*len(cell_content) ## Adding more phero if the cell has more than one ant
+
+        
         self.schedule.step()
+
+# class phero_grid(mesa.Model):
+
+#     def __init__(self, model_cologne:Model):
+
+#         self.grid = mesa.space.MultiGrid(model_cologne.grid.width,model_cologne.grid.height)
+
+#     def step(self):
+
+#         for
+        
+
+
+
+
+def print_grid(model):
+
+    agent_counts = np.zeros((model.grid.width, model.grid.height))
+
+    for cell in model.grid.coord_iter():
+        cell_content, x, y = cell
+        agent_count = len(cell_content)
+        agent_counts[x][y] = agent_count
+
+    plt.imshow(agent_counts, interpolation='nearest')
+    plt.colorbar()
+    plt.show()
 
 
         
+def agent_portrayal(agent):
+    portrayal = {
+    "Shape": "circle",
+    "Color": "red",
+    "Filled": "true",
+    "Layer": 0,
+    "r": 0.5,}
+    return portrayal
